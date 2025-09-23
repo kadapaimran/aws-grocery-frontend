f9add39cf22d506a9b6e0a77f390d19cbccb3b10
Kadapah Imran <kadapaimran99@hotmail.com>
2025-09-23 19:28:50 +0530

    Update Admin.jsx

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../styles/Admin.css";
 
-const BASE_URL = "http://localhost:8088/api/products";
+const BASE_URL = "http://ec2-18-188-177-237.us-east-2.compute.amazonaws.com:8088/api/products";
 const DEFAULT_IMAGE = "https://th.bing.com/th/id/OIP.wPjOsacGuRUjiOEUJmhRDgHaHa?w=179&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3";
 
 const Admin = () => {
